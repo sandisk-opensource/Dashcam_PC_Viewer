5a8cd3e91f46d2d70e9e1a680433edbb07a4176d
SanDisk OpenSource <opensource@sandisk.com>
2015-10-21 12:55:12 +0300

    Dashcam_PC_Viewer

---
+++ ffmpeg-20150921/libavcodec/pthread_frame.d
@@ -0,0 +1,17 @@
+libavcodec/pthread_frame.o: libavcodec/pthread_frame.c config.h compat/w32pthreads.h \
+ libavutil/attributes.h libavutil/common.h libavutil/attributes.h \
+ libavutil/version.h libavutil/macros.h libavutil/avconfig.h \
+ libavutil/intmath.h libavutil/x86/intmath.h libavutil/common.h \
+ libavutil/mem.h libavutil/error.h libavutil/avutil.h \
+ libavutil/rational.h libavutil/mathematics.h libavutil/intfloat.h \
+ libavutil/log.h libavutil/pixfmt.h libavutil/internal.h \
+ libavutil/timer.h libavutil/x86/timer.h libavutil/cpu.h libavutil/dict.h \
+ libavutil/x86/emms.h libavutil/cpu.h libavutil/libm.h \
+ libavutil/internal.h libavutil/mem.h libavcodec/avcodec.h \
+ libavutil/samplefmt.h libavutil/avutil.h libavutil/buffer.h \
+ libavutil/channel_layout.h libavutil/dict.h libavutil/frame.h \
+ libavutil/buffer.h libavutil/samplefmt.h libavutil/log.h \
+ libavutil/pixfmt.h libavutil/rational.h libavcodec/version.h \
+ libavutil/version.h libavcodec/internal.h libavutil/mathematics.h \
+ libavcodec/pthread_internal.h libavcodec/thread.h libavutil/avassert.h \
+ libavutil/opt.h
